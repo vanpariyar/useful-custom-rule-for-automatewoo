62421edd65b58888a10f2a59ad5ad844c5e03bd8
Ronak Vanpariya <vanpariyar@gmail.com>
2021-02-24 12:20:17 +0530

    first commit

---
+++ useful-custom-rule-for-automatewoo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Useful Custom Rule For Automatewoo
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     useful-custom-rule-for-automatewoo
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Useful_Custom_Rule_For_Automatewoo
+ */
+
+// Your code starts here.
